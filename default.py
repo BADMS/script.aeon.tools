85e11fe37860e25eb74b843133ba2618b7c75e4f
BADMS <dan@allskool.co.uk>
2017-02-04 17:16:17 +0000

    add threading

--- default.py
+++ default.py
@@ -3,6 +3,7 @@ import os
 import xbmc
 import xbmcgui
 import xbmcaddon
+from threading import Thread
 
 AEONT_ADDON = xbmcaddon.Addon()
 AEONT_ADDON_VERSION = AEONT_ADDON.getAddonInfo('version')
@@ -44,62 +45,25 @@ class Aeon_Tools_Main:
                     self.aeont_image_now_cpa = HOME.getProperty("cpa_aeon_fallback")
                 if self.aeont_image_now_cpa != self.aeont_image_prev_cpa:
                     self.aeont_image_prev_cpa = self.aeont_image_now_cpa
-                try:
-                    HOME.setProperty("Oldaeon_tools_Colorcpa", HOME.getProperty("aeon_tools_Colorcpa"))
-                    HOME.setProperty("Oldaeon_tools_CColorcpa", HOME.getProperty("aeon_tools_CColorcpa"))
-                    HOME.setProperty('aeon_tools_7978ImageUpdating', '0')
-                    Color_Only(self.aeont_image_now_cpa, "aeon_tools_Colorcpa", "aeon_tools_CColorcpa", "aeon_tools_FColorcpa")
-                except:
-                    HOME.setProperty('aeon_tools_7978ImageUpdating', '1')
-                    log("Could not process image for cpa daemon")
-                    HOME.setProperty('aeon_tools_7978ImageUpdating', '1')
+                    try:
+                        HOME.setProperty("Oldaeon_tools_Colorcpa", HOME.getProperty("aeon_tools_Colorcpa"))
+                        HOME.setProperty("Oldaeon_tools_CColorcpa", HOME.getProperty("aeon_tools_CColorcpa"))
+                        t = Thread(target=Color_Only, args=(self.aeont_image_now_cpa, "aeon_tools_Colorcpa", "aeon_tools_CColorcpa", "aeon_tools_FColorcpa"))
+                        t.start()
+                    except:
+                        log("Could not process image for cpa daemon")
             if not HOME.getProperty("cfa_aeon_set") == 'none':
                 self.aeont_image_now_cfa = xbmc.getInfoLabel("Control.GetLabel(7977)")
                 if self.aeont_image_now_cfa != self.aeont_image_prev_cfa:
                     self.aeont_image_prev_cfa = self.aeont_image_now_cfa
-
-                    HOME.setProperty("Oldaeon_tools_Colorcfa", HOME.getProperty("aeon_tools_Colorcfa"))
-                    HOME.setProperty("Oldaeon_tools_CColorcfa", HOME.getProperty("aeon_tools_CColorcfa"))
-                    HOME.setProperty('aeon_tools_7977ImageUpdating', '0')
-                    Color_Only(self.aeont_image_now_cfa, "aeon_tools_Colorcfa", "aeon_tools_CColorcfa", "aeon_tools_FColorcfa")
-
-                    HOME.setProperty('aeon_tools_7977ImageUpdating', '1')
-                    log("Could not process image for cfa daemon")
-                    HOME.setProperty('aeon_tools_7977ImageUpdating', '1')
-            self.aeont_image_now = xbmc.getInfoLabel("Player.Art(thumb)")
-            self.aeont_image_now_fa = xbmc.getInfoLabel("MusicPlayer.Property(Fanart_Image)")
-            if self.aeont_image_now != self.aeont_image_prev and xbmc.Player().isPlayingAudio():
-                try:
-                    self.aeont_image_prev = self.aeont_image_now
-                    imagecolor = Color_Only(self.aeont_image_now)
-                    HOME.setProperty("ImageColor1", imagecolor)
-                except:
-                    log("Could not process image for f daemon")
-            if self.aeont_image_now_fa != self.aeont_image_prev_fa and xbmc.Player().isPlayingAudio():
-                try:
-                    self.aeont_image_prev_fa = self.aeont_image_now_fa
-                    imagecolor = Color_Only(self.aeont_image_now_fa)
-                    HOME.setProperty("ImageColorfa1", imagecolor)
-                except:
-                    log("Could not process image for fa daemon")
-            xbmc.sleep(300)
-
-    def _StartInfoActions(self):
-        HOME.setProperty(self.prefix + 'aeon_tools_ImageUpdating', '0')
-        for info in self.infos:
-            if info == 'overall_color':
-                HOME.setProperty(self.prefix + "ImageOldColor", HOME.getProperty(self.prefix + "ImageColor"))
-                HOME.setProperty(self.prefix + "ImageOldCColor", HOME.getProperty(self.prefix + "ImageCColor"))
-                imagecolor = Color_Only(self.id)
-                HOME.setProperty(self.prefix + "ImageColor", imagecolor)
-                HOME.setProperty(self.prefix + "ImageCColor", Complementary_Color(imagecolor))
-            elif info == 'frequent_color':
-                HOME.setProperty(self.prefix + "ImageOldColor", HOME.getProperty(self.prefix + "ImageColor"))
-                HOME.setProperty(self.prefix + "ImageOldCColor", HOME.getProperty(self.prefix + "ImageCColor"))
-                imagecolor = Frequent_Color(self.id)
-                HOME.setProperty(self.prefix + "ImageColor", imagecolor)
-                HOME.setProperty(self.prefix + "ImageCColor", Complementary_Color(imagecolor))
-        HOME.setProperty(self.prefix + 'aeon_tools_ImageUpdating', '1')
+                    try:
+                        HOME.setProperty("Oldaeon_tools_Colorcfa", HOME.getProperty("aeon_tools_Colorcfa"))
+                        HOME.setProperty("Oldaeon_tools_CColorcfa", HOME.getProperty("aeon_tools_CColorcfa"))
+                        t = Thread(target=Color_Only, args=(self.aeont_image_now_cfa, "aeon_tools_Colorcfa", "aeon_tools_CColorcfa", "aeon_tools_FColorcfa"))
+                        t.start()
+                    except:
+                        log("Could not process image for cfa daemon")
+            xbmc.sleep(200)
 
     def _init_vars(self):
         self.window = xbmcgui.Window(10000)  # Home Window
@@ -127,12 +91,6 @@ class Aeon_Tools_Main:
                 continue
             elif arg.startswith('daemon='):
                 self.daemon = True
-            elif arg.startswith('prefix='):
-                self.prefix = arg[7:]
-                if not self.prefix.endswith("."):
-                    self.prefix = self.prefix + "."
-            elif arg.startswith('container='):
-                self.container = RemoveQuotes(arg[10:])
 
 class Aeon_Tools_Monitor(xbmc.Monitor):
 
